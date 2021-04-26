15c594b74a8562d41e8e9e4bdc059013d321cd7b
Miguel Neves <36202483+miguelj-neves@users.noreply.github.com>
2021-04-26 16:53:01 -0400

    Update __init__.py

--- EQTransformer/core/__init__.py
+++ EQTransformer/core/__init__.py
@@ -3,6 +3,7 @@ name='core'
 
 from .EqT_utils import *
 from .trainer import trainer
+from .trainer import _load_eqtmodel
 from .tester import tester
 from .predictor import predictor
 from .mseed_predictor import mseed_predictor
